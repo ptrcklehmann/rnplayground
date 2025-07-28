5a7bebfa8ea671ba2b5516b2aa58ca9a17d894a2
Patrick Lehmann <ptrcklehmann.com>
2025-07-28 18:29:21 +0200

    implement splashscreen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Navigation } from './src/navigation';
+import { useEffect } from 'react';
+import BootSplash from 'react-native-bootsplash';
 
 export default function App() {
+  useEffect(() => {
+    const hideSplash = async () => {
+      await BootSplash.hide({ fade: true });
+    };
+    hideSplash();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <Navigation />
--- ios/rnplayground/AppDelegate.swift
+++ ios/rnplayground/AppDelegate.swift
@@ -1,7 +1,8 @@
-import UIKit
+import RNBootSplash
 import React
-import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import React_RCTAppDelegate
+import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -12,7 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    didFinishLaunchingWithOptions launchOptions: [UIApplication
+      .LaunchOptionsKey: Any]? = nil
   ) -> Bool {
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
@@ -39,10 +41,15 @@ class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
   }
 
   override func bundleURL() -> URL? {
-#if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
-#else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
-#endif
+    #if DEBUG
+      RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    #else
+      Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+    #endif
+  }
+
+  override func customize(_ rootView: RCTRootView) {
+    super.customize(rootView)
+    RNBootSplash.initWithStoryboard("BootSplash", rootView: rootView)  // ⬅️ initialize the splash screen
   }
 }
